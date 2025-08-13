16044c40ec39c14b0563431a3f05834d360420d9
dahualiu <138657264+Design-hua@users.noreply.github.com>
2025-08-13 15:18:59 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/8.13/', // 这里就是你的 GitHub 仓库名
 });
