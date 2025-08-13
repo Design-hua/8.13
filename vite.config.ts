4c4c6f838a5937be5db9c26084de8c8251a8c125
dahualiu <138657264+Design-hua@users.noreply.github.com>
2025-08-13 16:34:07 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/8.13/', // 关键：告诉 Vite 构建资源路径
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
